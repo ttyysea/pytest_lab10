b0a9663bdb1ed28a9e2d6766d8445f005af43a91
tumtpk <pk.prateep@gmail.com>
2022-03-28 20:27:29 +0700

    news

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+
 from mangum import Mangum
-# from app.core import config
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": f"This is our secret key value: Secret Value" }
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
 
 handler = Mangum(app)
