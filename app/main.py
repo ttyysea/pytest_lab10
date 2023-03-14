34e0018bdda1f0ae6a1d6680f910823cdd1db07b
ttyysea <111670925+ttyysea@users.noreply.github.com>
2023-03-14 14:04:08 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,4 +12,8 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/hello/{name}")
+def read_name(name: str = None):
+    return {"hello": name}
+
 handler = Mangum(app)
